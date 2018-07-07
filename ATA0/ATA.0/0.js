4e47467392394e26bcc09bb4a705ad2524898170
Mustafa ÖZVER <mustafa.ozver@hotmail.com>
2018-07-07 14:43:23 +0300

    ATAid İşletim sistemi HTML arayüzü

---
+++ ATA0/ATA.0/0.js
@@ -0,0 +1,19 @@
+// ATA Dosya Yöneticisi Yönlendiricisi //
+
+Ana["Ad"] = "Dosya Yöneticisi";
+Ana["Deðiþkenler"] = Ana["Deðiþken"]["Oku"]();
+
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Pencere Arkasý Resmi"] = "Resimler/Pencere/pencere.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Pencere Gövdesi Arkasý Resmi"] = "Resimler/Pencere/pencere.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Yardým Tuþu"] = "Resimler/Pencere/y.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Yardým Tuþu Boyutu"] = 31;
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Küçültme Tuþu"] = "Resimler/Pencere/kc.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Küçültme Tuþu Boyutu"] = 31;
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Tam Boyut - Küçük Boyut Tuþu"] = "Resimler/Pencere/t.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Tam Boyut - Küçük Boyut Tuþu Boyutu"] = 31;
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Kapatma Tuþu"] = "Resimler/Pencere/k.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Kapatma Tuþu Boyutu"] =  31;
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Boyutlama Sürgüsü"] = "Resimler/Pencere/bos.png";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Görev Çubuðu Yüksekliði"] = 63;
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Görev Çubuðu Arka"] = "Resimler/Pencere/tsk.jpg";
+Ana["Deðiþkenler"]["ATA"]["Pencere Düzeni"]["Masaüstü Simge Büyüklüðü"] = 50;
